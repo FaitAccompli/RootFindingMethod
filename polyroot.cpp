a5f5e13c0507ca3396487001b5969c4c6ac5f6cb
FaitAccompli <raymart.montesclaros@gmail.com>
2020-03-06 17:00:14 +0800

    finished input

--- polyroot.cpp
+++ polyroot.cpp
@@ -4,7 +4,9 @@
 #include <vector>
 #include <cmath>
 #include <cstdlib>
+#include <algorithm>    // std::reverse
 #include <complex>
+
 
 using namespace std; 
 
@@ -27,8 +29,9 @@ int main (int argc, char *argv[])
 		getline(cin, fileName); 
 		cout<<"File name: "<<fileName<<endl; 
 	}
-	else 
+	else{
 		cout<<"Invalid Input"<<endl; 
+  }
 
 	
 
@@ -41,50 +44,72 @@ int main (int argc, char *argv[])
 		exit(1);  	
 
 	}
-	if (inFile.peek() == EOF) //Check if file is empty
-  	{
-    	cout << "The file is empty. Exiting..." << endl;
-    	system("pause");
-    	return (0);
-  	}
+  else 
+  {
+    while(inFile>>degree)
+    {
+      coeff.push_back(degree);   
+    }
+    reverse(coeff.begin(),coeff.end()); 
+    degree = coeff[coeff.size()-1];
+    coeff.pop_back(); 
+    system("cls"); 
+    string poly_out[coeff.size()];
+    int poly_outSize = sizeof(poly_out)/sizeof(poly_out[0]);
+    cout<<"Polynomial: "<<endl; 
+    for(int i = 0; i<poly_outSize; i++) 
+    {
+        poly_out[i] = to_string(coeff[i])+"x^"+to_string(coeff.size()-(i+1)); 
+
+    }
+
+    for(int i=0;i<poly_outSize;i++)
+    {
+      cout<<poly_out[i]<<endl; 
+    }
+  }
 
 
-  	if(isdigit(inFile.peek())) //check if first element is a digit. 
-  	{
-  		inFile>>degree; 
-  		for(int i=degree; i > 0 ; i--)
-  		{
-  			inFile >> coeff[i];
-  		}
-  	}
-  	else
-  	{
-  		cerr << "You inputted an invalid file."
-      	<< "Check proper input format" << endl;
-    	system("pause");
-    	exit(1);
-  	}
+  /*--------------------*/
+  // if (inFile.peek() == EOF) //Check if file is empty
+  // {
+  //   cout << "The file is empty. Exiting..." << endl;
+  //   system("pause");
+  //   return (0);
+  // }
+  // if(isdigit(inFile.peek())) //check if first element is a digit. 
+  // {
+  //   inFile>>degree; 
+  //   for(int i=degree; i > 0 ; i--)
+  //   {
+  //     inFile >> coeff[i];
+  //     cout<<"here";
+  //   }
+  // }
+  // else
+  // {
+  //   cerr << "You inputted an invalid file."
+  //        << "Check proper input format" << endl;
+  //   system("pause");
+  //   exit(1);
+  // }
+
+  //  /*--------------------*/
 
 
 
-  	/*Insert algorithm here*/
 
 
-  	//OUTPUT
-  	system('CLS'); 
-  	string poly_out[coeff.size()];
-  	cout<<"Polynomial: "<<endl; 
-  	for(int i = 0; i<poly_out.siz(); i++) 
-  	{
-  			poly_out[i] = to_string(coeff[i])+"x^"+to_string(coeff.size()-1); 
 
-  	}
 
-  	for(int i=0;i<poly_out.size();i++)
-  	{
 
-  		cout<<poly_out[i]<<endl; 
-  	}
+
+
+
+ //  	/*Insert algorithm here*/
+
+
+ //  	//OUTPUT
 
 	
 
